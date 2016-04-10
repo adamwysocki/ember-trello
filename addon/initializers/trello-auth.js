abd8cfaeb54ec95bab6d80e2d40b7c8d5dd2fb2c
Adam Wysocki <adam@adamwysocki.me>
2016-04-10 17:12:09 -0400

    took out initializers

--- addon/initializers/trello-auth.js
+++
@@ -1,9 +0,0 @@
-export function initialize(application) {
-  application.inject('route', 'trelloAuth', 'service:trello-auth');
-  application.inject('adapter', 'trelloAuth', 'service:trello-auth');
-}
-
-export default {
-  name: 'trello-auth',
-  initialize: initialize
-};
--- app/initializers/trello-auth.js
+++
@@ -1,6 +0,0 @@
-import { initialize } from 'ember-trello/initializers/trello-auth';
-
-export default {
-  name: 'trello-auth',
-  initialize: initialize
-};
