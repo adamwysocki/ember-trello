ccb2aa3844c9db140b269e94aadc156a0b58be36
Adam Wysocki <adam@adamwysocki.me>
2016-04-10 14:23:52 -0400

    first commit

--- index.js
+++ index.js
@@ -2,5 +2,14 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-trello'
+  name: 'ember-trello',
+
+  contentFor: function(type, config){
+    var emberTrelloConfig = config.emberTrello || {};
+
+    if (type === 'head' && emberTrelloConfig.key != null){
+      return `<script src="https://api.trello.com/1/client.js?key=${emberTrelloConfig.key}"></script>\n`;
+    }
+  }
+
 };
