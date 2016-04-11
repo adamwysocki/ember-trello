0bf59e39c97f4f5d12e194edd625741edc962091
Adam Wysocki <adam@adamwysocki.me>
2016-04-11 18:06:46 -0400

    board adapter

--- addon/initializers/trello-auth.js
+++ addon/initializers/trello-auth.js
@@ -1,7 +1,6 @@
-import Board from '../models/board';
+import { default as Board } from '../models/board';
 
 export function initialize() {
-
   let application = arguments[1] || arguments[0];
 
   application.inject('route', 'trelloAuth', 'service:trello-auth');
